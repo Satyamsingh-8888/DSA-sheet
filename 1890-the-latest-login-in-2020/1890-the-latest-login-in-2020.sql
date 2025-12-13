3744a3ea67fc96d88beaf61ae249241bd92515d0
Satyam Kumar <satyamkumarsingh8876@gmail.com>
2025-12-13 23:11:55 +0530

    Time: 563 ms (84.22%), Space: 0B (100%) - LeetHub

---
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+
+
+select distinct user_id, FIRST_VALUE(time_stamp) over (partition by user_id order by time_stamp desc) as last_stamp
+from logins where year(time_stamp)='2020';
