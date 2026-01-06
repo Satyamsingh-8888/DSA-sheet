3539cd004b1c7d2728ec319ffd67a92f19f1fa2f
Satyam Kumar <satyamkumarsingh8876@gmail.com>
2026-01-06 11:55:02 +0530

    Time: 3 ms (87.09%), Space: 52.4 MB (32.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -7,17 +7,15 @@ class Solution {
                     count++;
                     dfs(grid, i, j);
                 }
-
             }
         }
         return count;
     }
-
-    void dfs(char grid[][], int i, int j){
-        if(i<0||j<0||i>=grid.length||j>=grid.length) return;
+    void dfs(char[][] grid, int i, int j){
+        if(i<0||j<0||i>=grid.length|| j>=grid[0].length) return;
         if(grid[i][j]=='0') return;
+        grid[i][j]='0';
 
-        grid[i][j]=0;
         dfs(grid, i+1, j);
         dfs(grid, i-1, j);
         dfs(grid, i, j+1);
